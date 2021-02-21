c05a3819b47b18b3d1fd6cb01f553b5c2a73efdd
Madhubalajb <jbmadhumng@gmail.com>
2021-02-21 07:53:26 +0530

    Updates

---
+++ precache-manifest.e14e167efa3a8dd8baa9bc81cfe769e0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "c395eec9c3e98efe852550e69e0a2034",
+    "url": "/static/media/Phonebook.c395eec9.png"
+  },
+  {
+    "revision": "9214f76f387e3419d4b6",
+    "url": "/static/css/main.60074f4f.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ddaebb56fe93123a3bb52cc8a09e0bb5",
+    "url": "/static/media/mobile-track-down.ddaebb56.png"
+  },
+  {
+    "revision": "cc8a0659957f11cb32f0",
+    "url": "/static/js/2.73084df4.chunk.js"
+  },
+  {
+    "revision": "d553b5c163f4877c10c5600a25903ad0",
+    "url": "/static/media/coder.d553b5c1.webp"
+  },
+  {
+    "revision": "52a7598b2e67a43723049c2d61d0690f",
+    "url": "/static/media/ExpenseSplit.52a7598b.png"
+  },
+  {
+    "revision": "9214f76f387e3419d4b6",
+    "url": "/static/js/main.71c564b7.chunk.js"
+  },
+  {
+    "revision": "1d15327a2ffe1f7891272d2648dd5ae1",
+    "url": "/static/media/esMobile.1d15327a.png"
+  },
+  {
+    "revision": "2bfb1d12dd0f3f269b939fcc55a681d9",
+    "url": "/static/media/pbMobile.2bfb1d12.png"
+  },
+  {
+    "revision": "3f97ca0df42b0a1328796c005c2380dd",
+    "url": "/static/media/varshu.3f97ca0d.png"
+  },
+  {
+    "revision": "a407f69aeacf4f06e7e6a4c7f0a394db",
+    "url": "/static/media/varshuMobile.a407f69a.png"
+  },
+  {
+    "revision": "d5ff3b7f6a163ef3f40a072d5e0cb96f",
+    "url": "/static/media/track-down.d5ff3b7f.png"
+  },
+  {
+    "revision": "cc8a0659957f11cb32f0",
+    "url": "/static/css/2.e3abaf59.chunk.css"
+  },
+  {
+    "revision": "648c5f4c848d84091cd083c01d4e805d",
+    "url": "/index.html"
+  }
+];
